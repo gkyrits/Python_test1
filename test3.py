1d03bd8646143c34911c1413e3e4bd67003d905f
gkyrits <59622692+gkyrits@users.noreply.github.com>
2024-01-22 21:11:41 +0200

    fix test3

--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@ import tkinter as tk
 wroot = tk.Tk()
 wroot.title("test3 v01")
 tk.Label(wroot, text="main window").pack(pady=1)
-wroot.geometry("800x200+400+80")
+wroot.geometry("600x200+300+350")
 
 #wch1 = tk.Toplevel(wroot)
 #tk.Label(wch1, text="child 1 window" ).pack(padx=5, pady=10) 
@@ -14,7 +14,7 @@ wroot.geometry("800x200+400+80")
 
 wch3 = tk.Toplevel(wroot, borderwidth=1, bg="blue")
 tk.Label(wch3, text="child 3 window" ).pack(padx=80, pady=50)
-tk.Button(wch3, text="Exit", width=20).pack(side=tk.LEFT, padx=10)
+tk.Button(wch3, text="Exit", width=20, command=wch3.destroy).pack(side=tk.LEFT, padx=10)
 wch3.overrideredirect(1)
 wch3.geometry("400x200+100+100")
 
@@ -24,4 +24,5 @@ for relief in [tk.RAISED, tk.SUNKEN, tk.FLAT, tk.RIDGE, tk.GROOVE, tk.SOLID]:
     f.pack(side=tk.LEFT, padx=5, pady=5)
 
 wroot.mainloop()
+print("End...")
 
