e00d2197fd516004ef55f86bea271191f2d3b634
gkyr <gkyr@intracom-telecom.com>
2024-01-23 10:00:04 +0200

    test

--- test4.py
+++ test4.py
@@ -2,15 +2,15 @@ import tkinter as tk
 
 #---class App---
 class App:
-    def __init__(self,root,win):
-        tk.Button(root,text="Exit",command=win.destroy).place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    def __init__(self,frm,win):
+        tk.Button(frm,text="Exit",command=win.destroy).place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 #---main---
 root = tk.Tk()
 root.title('Test 4 V0.1')
 root.geometry("320x240+0+0")
-tk.Button(root, text='Test').pack(padx=5, pady=5, fill=tk.BOTH)
-#frm=tk.Frame(root, relief=tk.GROOVE, borderwidth=2).pack(padx=5, pady=5, fill=tk.BOTH)
-#win = App(frm,root)
+#root.overrideredirect=1
+frm=tk.Frame(root, relief=tk.GROOVE, borderwidth=2, height=1000).pack(padx=5, pady=5, fill=tk.BOTH)
+win = App(frm,root)
 tk.mainloop()
 print('End...')
