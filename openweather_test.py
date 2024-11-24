93836416f176c6a91d4dd48b716f1fbf41cada12
gkyrits <59622692+gkyrits@users.noreply.github.com>
2024-11-24 14:10:49 +0200

    add openweather test

---
+++ openweather_test.py
@@ -0,0 +1,25 @@
+import requests
+
+#user:gkyr@yahoo.gr  pass:gkyr1234
+#loc Nea Smyrni from google map
+#37.938209123871076, 23.709251306382026
+
+API_KEY = 'cc60f5942123b44409393d80500ce975'
+
+parameters = {'appid': API_KEY,
+              'lat': '37.93820','lon': '23.70925','units': 'metric'}
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+data = requests.get(url, parameters).json()
+
+print('')
+print('Place: {} {}'.format(data['name'],data['sys']['country']))
+print(data['weather'][0]['description'])
+print('Temperature: {} °C'.format(data['main']['temp']))
+print('Feels Like : {} °C'.format(data['main']['feels_like']))
+print('Humidity   : {} %'.format(data['main']['humidity']))
+print('Pressure   : {} hPa'.format(data['main']['pressure']))
+print('Wind       : {} m/s'.format(data['wind']['speed']))
+print('')
+#print('--------------')
+#print(data)
