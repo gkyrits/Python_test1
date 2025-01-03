cb3a8399d5297246a88e707a4d7f43ac463bb05b
gkyr <gkyrits@yahoo.gr>
2025-01-03 15:44:26 +0200

    add MPL3115 sensor

--- rgb2_test.py
+++ rgb2_test.py
@@ -1,26 +1,19 @@
-from gpiozero import LED,RGBLED,Buzzer
+from gpiozero import RGBLED,Buzzer
 from time import sleep
 
 led = RGBLED(red=29, green=28, blue=30)
-bz = Buzzer(31)
+bz  = Buzzer(31)
 
+VAL = 10
 
-TIME=4.0
-MAX_RED=20
-MAX_GREEN=15
-MAX_BLUE=100
-
-red_wait=TIME/MAX_RED
-green_wait=TIME/MAX_GREEN
-blue_wait=TIME/MAX_BLUE
-
-colors = {"white":(255,255,255), "red":(255,0,0), "green":(0,255,0), "blue":(0,0,255),
-          "yellow":(255,255,0), "aqua":(0,255,255), "fuchsia":(255,0,255)
-          ,"black":(0,0,0)
+colors = {"white":(VAL,VAL,VAL), "red":(VAL,0,0), "green":(0,VAL,0), "blue":(0,0,VAL),
+          "yellow":(VAL,VAL,0), "aqua":(0,VAL,VAL), "fuchsia":(VAL,0,VAL),
+          "black":(0,0,0)
           }
 
 def play_colors():
-    for col in colors.keys():
+    col_list = list(colors.keys())
+    for col in col_list[:7]:
         print("%s" % col)
         led.color = (colors[col][0]/255,colors[col][1]/255,colors[col][2]/255)        
         sleep(1)
