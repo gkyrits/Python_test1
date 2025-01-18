e887900aec58d85c1742fad5ce4cf1f9d8fb8d6f
gkyrits <59622692+gkyrits@users.noreply.github.com>
2025-01-18 11:17:55 +0200

    [raspi_play.py] format save data

--- weather_time/repository.py
+++ weather_time/repository.py
@@ -1,22 +1,33 @@
-
 import time
 
-FILE='sensor' #file name to save info
+FILE = 'sensor'  # file name to save info
 
-info = {'sens1':{}, 'sens2':{}, 'sens3':{}}
+info = {'sens1': {}, 'sens2': {}, 'sens3': {}}
 
-#get year_moth string
+# get year_month string
 def __get_year_month():
     return time.strftime('%Y-%m', time.localtime())
 
-#save info with timestamb to file in a line
+# format floats in the dictionary to 1 decimal point
+def format_floats(data):
+    if isinstance(data, dict):
+        return {k: format_floats(v) for k, v in data.items()}
+    elif isinstance(data, float):
+        return f"{data:.1f}"
+    elif isinstance(data, list):
+        return [format_floats(i) for i in data]
+    else:
+        return data
+
+# save info with timestamp to file in a line
 def save_info():
     try:
-        with open(FILE+'-'+__get_year_month() , 'a') as f:
-            f.write(time.strftime('%d %H:%M:%S', time.localtime())+' ')
-            f.write(str(info)+'\n')
-    except: 
-        print('Fail to save info to file')
+        formatted_info = format_floats(info)
+        with open(FILE + '-' + __get_year_month(), 'a') as f:
+            f.write(time.strftime('%d %H:%M:%S', time.localtime()) + ' ')
+            f.write(str(formatted_info) + '\n')
+    except Exception as e:
+        print('Fail to save info to file:', e)
 
 
 
